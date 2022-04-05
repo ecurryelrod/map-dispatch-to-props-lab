c8b942efbef221b84bb5f9bbf0c1f9b8a4a988c8
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-05 16:42:10 -0600

    commit tree 01d4ce829185e788f224c9c5c5ada8e7b97f5a77

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
 
-export class RestaurantInput extends Component {
+class RestaurantInput extends Component {
 
   state = {
     name: '',
@@ -24,6 +24,7 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -49,6 +50,12 @@ export class RestaurantInput extends Component {
   }
 };
 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addRestaurant: restaurant => dispatch(addRestaurant(restaurant))
+//   }
+// }
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, {addRestaurant})(RestaurantInput)
+// export default connect(null, mapDispatchToProps)(RestaurantInput)
